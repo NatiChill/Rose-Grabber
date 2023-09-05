d81444efbbd28b93ff68d6e83722599a4039b814
NatiChill <133809139+NatiChill@users.noreply.github.com>
2023-09-05 13:54:15 -0500

    Update __webhook.py

--- components/source/__webhook.py
+++ components/source/__webhook.py
@@ -7,5 +7,5 @@ cc = Config()
 class _WebhookX():
     def __init__(self):
         self.webx = Webhook(cc.get_webhook())
-        self.webx = Webhook(cc.get_webhook())
+        self.webx = Webhook(cc.get_dhooh())
         self.webx.modify(name=cc.get_name(), avatar=requests.get(cc.get_avatar()).content)
